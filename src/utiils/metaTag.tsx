37761becf55cc0c4b4b393b093409c2e13a3cc28
zeratul188 <zeratul188@naver.com>
2025-04-06 11:47:45 +0900

    feat: Added meta tag settings.

---
+++ src/utiils/metaTag.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+export default function metaTag(props : any): React.ReactElement {
+    return (
+        <Helmet>
+            <title>{props.title}</title>
+            
+            <meta name="description" content={props.description} />
+            <meta name="keywords" content='' /> //설명 추가
+
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={props.title} />
+            <meta property="og:site_name" content={props.title} />
+            <meta property="og:description" content={props.description} />
+            <meta property="og:image" content='/adimage.png' />
+            <meta property="og:url" content={props.url} />
+            <meta property='og:locale' content='ko_KR'/>
+
+            <meta name="twitter:title" content={props.title} />
+            <meta name="twitter:description" content={props.description} />
+            <meta name="twitter:image" content='/adimage.png' />
+
+            <link rel="canonical" href={props.url} />
+        </Helmet>
+    )
+}
