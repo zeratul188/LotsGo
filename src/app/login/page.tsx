b8113507317a18ce4b52f0b372b5a521ebd8395c
zeratul188 <zeratul188@naver.com>
2025-05-08 16:11:29 +0900

    feat: Added settings related to login status.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,25 @@
 'use client'
+import { useEffect } from "react";
 import { InputsComponent, LogoComponent } from "./LoginForm";
 import { useLoginForm } from "./LoginForm";
+import { addToast } from "@heroui/react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
     const loginForm = useLoginForm();
+    const router = useRouter();
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            addToast({
+                title: "로그인 되어있음",
+                description: `이미 로그인이 완료되어 있습니다.`,
+                color: "danger"
+            });
+            router.back();
+        }
+    }, []);
 
     return (
         <div className="min-h-[calc(100vh-65px)] flex justify-center items-center flex-col p-5 sm:p-0">
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,25 @@
 'use client'
+import { useEffect } from "react";
 import { LogoComponent, InputsComponent, useSignupForm } from "./SignupForm"
 import { Divider } from "@heroui/react";
+import { addToast } from "@heroui/react";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
     const signupForm = useSignupForm();
+    const router = useRouter();
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            addToast({
+                title: "로그인 되어있음",
+                description: `이미 로그인이 완료되어 있습니다.`,
+                color: "danger"
+            });
+            router.back();
+        }
+    }, []);
 
     return (
         <div className="min-h-[calc(100vh-65px)] sm:max-w-[640px] w-full mx-auto px-4 box-border">
