7990337c8d8dc63673ff1b41b8397a2b33a09173
zeratul188 <zeratul188@naver.com>
2025-06-10 17:12:28 +0900

    feat: Improved expedition list colors.

--- src/app/character/ExpeditionForm.tsx
+++ src/app/character/ExpeditionForm.tsx
@@ -16,9 +16,9 @@ export function ExpeditionsComponent({ expeditions }: ExpeditionComponentProps) 
                     <div className="max-w-full overflow-x-auto scrollbar-hide flex gap-3 mt-2">
                         {data.levels.map((item, idx) => (
                             <Chip key={idx} variant="flat" radius="sm" className={clsx(
-                                `${getBgColorByLevels(item.level)} text-white items-center`,
+                                `${getBgColorByLevels(item.level)} items-center`,
                                 getCountByLevel(item.level, idx === 0 ? 9999 : data.levels[idx-1].level, expeditions.filter(character => character.server === server)) > 0 ? 'flex' : 'hidden'
-                            )}><span className="text-[#f0f0f0] mr-1">{item.level}+ : </span>{getCountByLevel(item.level, idx === 0 ? 9999 : data.levels[idx-1].level, expeditions.filter(character => character.server === server))}</Chip>
+                            )}><span className="mr-1">{item.level}+ : </span>{getCountByLevel(item.level, idx === 0 ? 9999 : data.levels[idx-1].level, expeditions.filter(character => character.server === server))}</Chip>
                         ))}
                     </div>
                     <Divider className="mt-2 mb-2"/>
@@ -33,7 +33,7 @@ export function ExpeditionsComponent({ expeditions }: ExpeditionComponentProps) 
                                 onPress={() => handleSelectCharacter(character.nickname)}>
                                 <CardBody>
                                     <div className="w-full flex gap-4 items-center">
-                                        <Avatar isBordered size="md" src={getImgByJob(character.job)}/>
+                                        <Avatar isBordered size="md" src={getImgByJob(character.job)} className="border-[#ff968a]"/>
                                         <div>
                                             <p className="truncate overflow-hidden whitespace-nowrap">{character.nickname}</p>
                                             <p className="fadedtext truncate overflow-hidden whitespace-nowrap text-[10pt]">Lv.{character.level} · {character.job}</p>
