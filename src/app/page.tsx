7f21507bc12fc9b9bd2520083807f9f5a627475d
zeratul188 <zeratul188@naver.com>
2025-06-10 11:40:35 +0900

    feat: Added title element.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,18 @@ import * as React from "react";
 import CalendarComponent from "./home/CalendarForm";
 import ChecklistComponent from "./home/ChecklistForm";
 import { TodoComponent } from "./home/TodoForm";
+import TitleComponent from "./home/TitleForm";
 
 export default function Home() {
 
   return (
-    <>
-      <div className="min-h-[calc(100vh-65px)] p-5 w-full max-w-[1280px] mx-auto pb-20">
+    <div className="w-full min-h-[calc(100vh-65px)]">
+      <TitleComponent/>
+      <div className="p-5 w-full max-w-[1280px] mx-auto pb-20">
         <ChecklistComponent/>
         <TodoComponent/>
         <CalendarComponent/>
       </div>
-    </>
+    </div>
   );
 }
