832995c6f63b149195a87211e3a14d082dbaa443
zeratul188 <zeratul188@naver.com>
2025-07-07 08:10:07 +0900

    feat: Improved meta description.

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -3,7 +3,7 @@ import CalendarClient from "./CalendarClient";
 
 export const metadata: Metadata = {
     title: '일정 · 로츠고 Lot\'s Go',
-    description: '로스트아크의 일정을 관리하세요.',
+    description: '캐릭터별 숙제뿐 아니라 길드 일정도 함께 관리하세요. 로스트아크 플레이를 더 효율적으로 도와주는 일정 관리 기능입니다.',
 };
 
 export default function Calendar() {
--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -3,7 +3,7 @@ import CharacterClient from "./CharacterClient";
 
 export const metadata: Metadata = {
     title: '전투정보실 · 로츠고 Lot\'s Go',
-    description: '로츠고에서 캐릭터 정보를 확인하세요.',
+    description: '로스트아크의 캐릭터 장비, 보석, 트라이포드, 카드 세팅 등 상세 정보를 확인할 수 있어요.',
 };
 
 export default function Character() {
