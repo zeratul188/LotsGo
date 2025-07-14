70b36667edf07156981ab50200f690e8742743aa
zeratul188 <zeratul188@naver.com>
2025-07-14 11:55:32 +0900

    feat: Improved the remaining homework status style.

--- src/app/home/checklistFeat.ts
+++ src/app/home/checklistFeat.ts
@@ -2,6 +2,15 @@ import { SetStateFn } from "@/utiils/utils";
 import { LoginUser } from "../store/loginSlice";
 import { addToast } from "@heroui/react";
 import { CheckCharacter } from "../store/checklistSlice";
+import { Boss } from "../api/checklist/boss/route";
+
+export type ChecklistData = {
+    nickname: string,
+    level: number,
+    isGold: boolean,
+    contentName: string,
+    difficulty: string
+}
 
 // 로그인 여부 확인 함수
 export function isLogin(): boolean {
@@ -16,7 +25,9 @@ export function isLogin(): boolean {
 // 숙제 데이터 불러오는 함수
 export async function loadChecklist(
     setChecklist: SetStateFn<CheckCharacter[]>,
-    setLoading: SetStateFn<boolean>
+    setLoading: SetStateFn<boolean>,
+    setDatas: SetStateFn<ChecklistData[]>,
+    bosses: Boss[]
 ) {
     const userStr = localStorage.getItem('user');
     const storedUser: LoginUser = userStr ? JSON.parse(userStr) : null;
@@ -34,6 +45,24 @@ export async function loadChecklist(
     }
 
     const checklist: CheckCharacter[] = await res.json();
+    const datas: ChecklistData[] = [];
+
+    for (const character of checklist) {
+        for (const content of character.checklist) {
+            if (!content.isCheck && !content.isDisable) {
+                const newData: ChecklistData = {
+                    nickname: character.nickname,
+                    level: character.level,
+                    contentName: content.name,
+                    difficulty: content.difficulty,
+                    isGold: content.isGold
+                }
+                datas.push(newData);
+            }
+        }
+    }
+
+    datas.sort((a, b) => getLevelByContent(bosses, b.contentName, b.difficulty) - getLevelByContent(bosses, a.contentName, a.difficulty));
     checklist.sort((a, b) => b.level - a.level);
     checklist.sort((a, b) => {
         if (a.isGold && !b.isGold) return -1;
@@ -41,27 +70,20 @@ export async function loadChecklist(
         return 0;
     });
     checklist.sort((a, b) => a.position - b.position);
+    setDatas(datas);
     setChecklist(checklist);
     setLoading(false);
 }
 
-// 숙제가 남아있는지 확인 여부
-export function getUnfinishedChecklist(checklist: CheckCharacter[]): CheckCharacter[] {
-    return checklist.filter((character) => {
-        for (const item of character.checklist) {
-            if (item.isGold && !item.isCheck) return true;
-        }
-        return false;
-    });
-}
-
-// 숙제가 남은 특정 캐릭터의 콘텐츠 반환 함수
-export function getUnfinishedContents(character: CheckCharacter): string[] {
-    const list: string[] = [];
-    for (const item of character.checklist) {
-        if (!item.isCheck && item.isGold) {
-            list.push(`${item.name} ${item.difficulty}`);
+function getLevelByContent(bosses: Boss[], contentName: string, difficulty: string): number {
+    for (const boss of bosses) {
+        if (boss.name === contentName) {
+            for (const diff of boss.difficulty) {
+                if (diff.difficulty === difficulty) {
+                    return diff.level;
+                }
+            }
         }
     }
-    return list;
+    return 0;
 }
