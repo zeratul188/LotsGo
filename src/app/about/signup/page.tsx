349771b4fcd44dcb67c30235441f9b35a87858d5
zeratul188 <zeratul188@naver.com>
2025-07-17 23:41:54 +0900

    feat: Added member registration guide element configuration.

--- src/app/about/signup/page.tsx
+++ src/app/about/signup/page.tsx
@@ -1,7 +1,8 @@
 export default function Signup() {
     return (
-        <div className="w-full">
-            회원가입
+        <div className="w-full [&_p]:text-lg [&_li]:text-lg [&_h3]:text-xl [&_h1]:text-3xl">
+            <h1 className="font-bold">로츠고 회원가입</h1>
+            
         </div>
     )
 }
