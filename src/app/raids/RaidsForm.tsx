b9d1f8e7f86d89b9473c8938bb06d67ad171f6fd
zeratul188 <zeratul188@naver.com>
2025-10-02 10:14:24 +0900

    Added a party raid list feature. (In progress)2

--- src/app/raids/RaidsForm.tsx
+++ src/app/raids/RaidsForm.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { Boss } from "../api/checklist/boss/route";
 import { Party, Raid } from "../api/raids/route";
 import { loadPartys } from "./raidsFeat";
-import { Button, Input, Select, Selection, SelectItem } from "@heroui/react";
-import { useMobileQuery } from "@/utiils/utils";
+import { Button, Input, Modal, ModalBody, ModalContent, ModalHeader, Select, Selection, SelectItem } from "@heroui/react";
+import { SetStateFn, useMobileQuery } from "@/utiils/utils";
 
 // 파티 내 레이드 목록 컴포넌트
 type PartyRaidsComponentProps = {
@@ -17,9 +17,33 @@ export function PartyRaidsComponent({selectedParty, bosses}: PartyRaidsComponent
     const [searchValue, setSearchValue] = useState(''); 
     const isMobile = useMobileQuery();
 
+    const [isOpenAdd, setOpenAdd] = useState(false);
+
     useEffect(() => {
         loadPartys(selectedParty, setPartys, setResults);
     }, [selectedParty]);
+
+    useEffect(() => {
+        const valueList = Array.from(searchContent);
+        if (valueList.length === 0) {
+            setResults(partys);
+        } else {
+            const selectedIndex = Number(valueList[0]);
+            const selectedBoss = bosses.sort((a, b) => {
+                const bDiff = bosses.find(boss => boss.name === b.name);
+                const aDiff = bosses.find(boss => boss.name === a.name);
+                let bValue = 0, aValue = 0;
+                if (bDiff){
+                    bValue = Math.min(...bDiff.difficulty.map(diff => diff.level));
+                }
+                if (aDiff) {
+                    aValue = Math.min(...aDiff.difficulty.map(diff => diff.level));
+                }
+                return bValue - aValue;
+            })[selectedIndex];
+            setResults(partys.filter(party => party.content === selectedBoss.name));
+        }
+    }, [searchContent]);
 
     return (
         <div className="w-full">
@@ -72,5 +96,33 @@ export function PartyRaidsComponent({selectedParty, bosses}: PartyRaidsComponent
                 </Button>
             </div>
         </div>
+    )
+}
+
+// 파티 추가 Modal
+type AddPartyModalProps = {
+    isOpenAdd: boolean,
+    setOpenAdd: SetStateFn<boolean>,
+    partys: Party[],
+    setPartys: SetStateFn<Party[]>
+}
+function AddPartyModal({ isOpenAdd, setOpenAdd, partys, setPartys }: AddPartyModalProps) {
+    return (
+        <Modal 
+            radius="sm"
+            isOpen={isOpenAdd}>
+            <ModalContent>
+                {(onClose) => (
+                    <>
+                        <ModalHeader>파티 추가</ModalHeader>
+                        <ModalBody>
+                            <div className="w-full max-h-[500px] sm:max-h-[800px] overflow-y-auto scroll-auto">
+                                 
+                            </div>
+                        </ModalBody>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
     )
 }
