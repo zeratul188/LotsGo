2a59189e2da61c77a7d7312dc38f537283c60277
zeratul188 <zeratul188@naver.com>
2026-02-16 08:56:27 +0900

    fixed: Fixed export issue.

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,6 +4,7 @@ import { isMatchValue } from "@/utiils/bcrypt";
 import type { Character } from "@/app/store/loginSlice";
 import { addDoc, collection, getDocs, limit, query, Timestamp, where } from "firebase/firestore";
 import { generateRefreshToken, hashToken, signAccessToken } from "@/lib/auth";
+import { getClientIp } from "./loginFeat";
 
 export type User = {
     id: string,
@@ -90,23 +91,4 @@ export async function POST(req: NextRequest) {
         }
         return NextResponse.json({ type: 'null', error: '데이터 처리 중 문제가 발생하였습니다.' }, { status: 500 });
     }
-}
-
-export function getClientIp(req: NextRequest) {
-    const xff = req.headers.get("x-forwarded-for");
-    let ip = (xff ? xff.split(",")[0].trim() : null) ||
-        req.headers.get("x-real-ip")?.trim() ||
-        req.headers.get("cf-connecting-ip")?.trim() ||
-        "unknown";
-
-    // localhost IPv6
-    if (ip === "::1") return "127.0.0.1";
-
-    // IPv4-mapped IPv6 (::ffff:123.123.123.123) → IPv4만 뽑기
-    if (ip.startsWith("::ffff:")) ip = ip.replace("::ffff:", "");
-
-    // 혹시 포트가 붙는 케이스(드물지만) "1.2.3.4:12345" → "1.2.3.4"
-    ip = ip.replace(/:\d+$/, "");
-
-    return ip;
 }
