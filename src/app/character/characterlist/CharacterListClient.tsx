ef0d4672060502580183ee9b7b09b6f168281b11
zeratul188 <zeratul188@naver.com>
2026-03-02 18:49:24 +0900

    style: 페이지 안내 문구 추가.

--- src/app/character/characterlist/CharacterListClient.tsx
+++ src/app/character/characterlist/CharacterListClient.tsx
@@ -47,6 +47,11 @@ export default function CharacterListClient() {
 
     return (
         <div className="min-h-[calc(100vh-65px)] p-5 w-full max-w-[1280px] mx-auto">
+            <h1 className="text-3xl font-bold">원정대 모아보기</h1>
+            <div className="mt-2 mb-4">
+                <p className="text-sm fadedtext">원정대 캐릭터의 핵심 스펙을 한눈에 간략하게 확인할 수 있는 페이지입니다.</p>
+                <p className="text-xs fadedtext">전투정보실에서 최소 1회 이상 조회한 캐릭터만 표시되며, 전투정보실 조회 시점에 저장된 데이터만 노출됩니다.</p>
+            </div>
             <div className="w-full grid min-[812px]:grid-cols-2 min-[1224px]:grid-cols-3 gap-3">
                 {expeditionCharacters.map((character) => (
                     <Card key={character.id} fullWidth radius="sm">
