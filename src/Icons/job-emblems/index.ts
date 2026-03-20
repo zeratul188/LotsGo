803eab064b655fe2f5ae87af9ee9085359aab40f
zeratul188 <zeratul188@naver.com>
2026-03-20 20:45:08 +0900

    feat: 직업 엠블렘 추가.

---
+++ src/Icons/job-emblems/index.ts
@@ -0,0 +1,67 @@
+import type { ComponentType, SVGProps } from "react";
+
+import JobEmblem01 from "./generated/JobEmblem01";
+import JobEmblem02 from "./generated/JobEmblem02";
+import JobEmblem03 from "./generated/JobEmblem03";
+import JobEmblem04 from "./generated/JobEmblem04";
+import JobEmblem05 from "./generated/JobEmblem05";
+import JobEmblem06 from "./generated/JobEmblem06";
+import JobEmblem07 from "./generated/JobEmblem07";
+import JobEmblem08 from "./generated/JobEmblem08";
+import JobEmblem09 from "./generated/JobEmblem09";
+import JobEmblem10 from "./generated/JobEmblem10";
+import JobEmblem11 from "./generated/JobEmblem11";
+import JobEmblem12 from "./generated/JobEmblem12";
+import JobEmblem13 from "./generated/JobEmblem13";
+import JobEmblem14 from "./generated/JobEmblem14";
+import JobEmblem15 from "./generated/JobEmblem15";
+import JobEmblem16 from "./generated/JobEmblem16";
+import JobEmblem17 from "./generated/JobEmblem17";
+import JobEmblem18 from "./generated/JobEmblem18";
+import JobEmblem19 from "./generated/JobEmblem19";
+import JobEmblem20 from "./generated/JobEmblem20";
+import JobEmblem21 from "./generated/JobEmblem21";
+import JobEmblem22 from "./generated/JobEmblem22";
+import JobEmblem23 from "./generated/JobEmblem23";
+import JobEmblem24 from "./generated/JobEmblem24";
+import JobEmblem25 from "./generated/JobEmblem25";
+import JobEmblem26 from "./generated/JobEmblem26";
+import JobEmblem27 from "./generated/JobEmblem27";
+import JobEmblem28 from "./generated/JobEmblem28";
+import JobEmblem29 from "./generated/JobEmblem29";
+
+export type JobEmblemComponent = ComponentType<SVGProps<SVGSVGElement> & { size?: number }>;
+
+export const jobEmblemMap = {
+    "\uac00\ub514\uc5b8\ub098\uc774\ud2b8": JobEmblem01,
+    "\uac74\uc2ac\ub9c1\uc5b4": JobEmblem02,
+    "\uae30\uacf5\uc0ac": JobEmblem03,
+    "\uae30\uc0c1\uc220\uc0ac": JobEmblem04,
+    "\ub370\ubaa8\ub2c9": JobEmblem05,
+    "\ub370\ube4c\ud5cc\ud130": JobEmblem06,
+    "\ub3c4\ud654\uac00": JobEmblem07,
+    "\ub514\uc2a4\ud2b8\ub85c\uc774\uc5b4": JobEmblem08,
+    "\ub9ac\ud37c": JobEmblem09,
+    "\ubc14\ub4dc": JobEmblem10,
+    "\ubc1c\ud0a4\ub9ac": JobEmblem11,
+    "\ubc30\ud2c0\ub9c8\uc2a4\ud130": JobEmblem12,
+    "\ubc84\uc11c\ucee4": JobEmblem13,
+    "\ube0c\ub808\uc774\ucee4": JobEmblem14,
+    "\ube14\ub798\uc2a4\ud130": JobEmblem15,
+    "\ube14\ub808\uc774\ub4dc": JobEmblem16,
+    "\uc11c\uba38\ub108": JobEmblem17,
+    "\uc18c\uc11c\ub9ac\uc2a4": JobEmblem18,
+    "\uc18c\uc6b8\uc774\ud130": JobEmblem19,
+    "\uc2a4\uce74\uc6b0\ud130": JobEmblem20,
+    "\uc2a4\ud2b8\ub77c\uc774\ucee4": JobEmblem21,
+    "\uc2ac\ub808\uc774\uc5b4": JobEmblem22,
+    "\uc544\ub974\uce74\ub098": JobEmblem23,
+    "\uc6cc\ub85c\ub4dc": JobEmblem24,
+    "\uc778\ud30c\uc774\ud130": JobEmblem25,
+    "\ucc3d\uc220\uc0ac": JobEmblem26,
+    "\ud638\ud06c\uc544\uc774": JobEmblem27,
+    "\ud640\ub9ac\ub098\uc774\ud2b8": JobEmblem28,
+    "\ud658\uc218\uc0ac": JobEmblem29,
+} as const;
+
+export type JobEmblemName = keyof typeof jobEmblemMap;
